c150fa9819efa5828df9964a1213a320ca95b400
unknown <taabishshaikh2>
2023-10-28 15:34:20 +0530

    added the assets in public folder

--- src/data/events.jsx
+++ src/data/events.jsx
@@ -1,8 +1,8 @@
 const events = {
   Ar_Sports: {
     img: [
-      "/dist/assets/Simply Gaming Event Galleria/AR Sports Experience/IMG20221120192100.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/AR Sports Experience/IMG20221121120512.jpg",
+      "/assets/Simply Gaming Event Galleria/AR Sports Experience/IMG20221120192100.jpg",
+      "/assets/Simply Gaming Event Galleria/AR Sports Experience/IMG20221121120512.jpg",
     ],
     title: "AR Sports Experience",
     desc: "An AR (Augmented Reality) event experience is an immersive and interactive encounter that combines the physical world with digital elements to provide attendees with a unique and engaging event.",
@@ -13,9 +13,9 @@ const events = {
 
   Flight_Simulator: {
     img: [
-      "/dist/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20230901-WA0007.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/AR Sports Experience/IMG20221120192100.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20220731-WA0005.jpg",
+      "/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20230901-WA0007.jpg",
+      "/assets/Simply Gaming Event Galleria/AR Sports Experience/IMG20221120192100.jpg",
+      "/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20220731-WA0005.jpg",
     ],
     title: "Flight Simulator",
     desc: " A flight simulator event experience combines the excitement of aviation with the interactive and immersive nature of flight simulators. These events are designed to provide participants with a realistic and memorable simulation of piloting an aircraft",
@@ -25,21 +25,21 @@ const events = {
   },
   // Gaming_PC: {
   //   img: [
-  //     "/dist/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20220713-WA0002.jpg",
-  //     "/dist/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20230901-WA0007.jpg",
-  //     "/dist/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20220731-WA0005.jpg",
-  //     "/dist/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20230901-WA0013.jpg",
-  //     "/dist/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG_20230618_004415.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20220713-WA0002.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20230901-WA0007.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20220731-WA0005.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20230901-WA0013.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG_20230618_004415.jpg",
   //   ],
   //   title: "Gaming PC Setup",
   //   desc: "A gaming PC event experience is an immersive gathering or exhibition centered around gaming, PC hardware, and the gaming community. These events can vary in scale from small local meetups to large conventions and provide attendees with opportunities to explore the latest gaming technologies, play video games, interact with industry professionals, and connect with fellow gamers",
   // },
   Mario_Kart: {
     img: [
-      "/dist/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG-20220407-WA0001.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20230901-WA0013.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20230901-WA0007.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20220731-WA0005.jpg",
+      "/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG-20220407-WA0001.jpg",
+      "/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20230901-WA0013.jpg",
+      "/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20230901-WA0007.jpg",
+      "/assets/Simply Gaming Event Galleria/Gaming PC Setup/IMG-20220731-WA0005.jpg",
     ],
     center:{ lat:12.9716448, //bangalorr
       long:77.594554
@@ -49,11 +49,11 @@ const events = {
   },
   PlayStation: {
     img: [
-      "/dist/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG-20220303-WA0024.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG-20220306-WA0012.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG-20220407-WA0001.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG-20220407-WA0003.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG_20211224_182632.jpg",
+      "/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG-20220303-WA0024.jpg",
+      "/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG-20220306-WA0012.jpg",
+      "/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG-20220407-WA0001.jpg",
+      "/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG-20220407-WA0003.jpg",
+      "/assets/Simply Gaming Event Galleria/PlayStation 4-5/IMG_20211224_182632.jpg",
     ],
     title: "PlayStation 4/5",
     desc: "PlayStation events are gatherings or exhibitions organized by Sony Interactive Entertainment to showcase the latest developments, games, and hardware related to the PlayStation gaming platform.",
@@ -63,11 +63,11 @@ const events = {
   },
   Racing_Simulator: {
     img: [
-      "/dist/assets/Simply Gaming Event Galleria/Racing Simulator/IMG_20230526_124125.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Racing Simulator/20230811_131650.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Racing Simulator/IMG-20230804-WA0014.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Racing Simulator/IMG-20230906-WA0000.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Racing Simulator/IMG_20220703_191203.jpg",
+      "/assets/Simply Gaming Event Galleria/Racing Simulator/IMG_20230526_124125.jpg",
+      "/assets/Simply Gaming Event Galleria/Racing Simulator/20230811_131650.jpg",
+      "/assets/Simply Gaming Event Galleria/Racing Simulator/IMG-20230804-WA0014.jpg",
+      "/assets/Simply Gaming Event Galleria/Racing Simulator/IMG-20230906-WA0000.jpg",
+      "/assets/Simply Gaming Event Galleria/Racing Simulator/IMG_20220703_191203.jpg",
     ],
     center:{ lat:17.3850592, //Hyderabad
       long:78.4867188
@@ -77,14 +77,14 @@ const events = {
   },
   Virtual_Reality: {
     img: [
-      "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR + Car Driving Experience/IMG-20230709-WA0005.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Beat Saber/IMG-20230609-WA0006.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Beat Saber/IMG-20230706-WA0002.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Beat Saber/IMG_20230809_164434.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Cricket Experience/IMG_20230617_165033.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Rollercoaster Experience/IMG_20230121_151214.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/Walk the Plank Experience/IMG_20230809_164407.jpg",
-      "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/Walk the Plank Experience/IMG_20230809_164423.jpg",
+      "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR + Car Driving Experience/IMG-20230709-WA0005.jpg",
+      "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Beat Saber/IMG-20230609-WA0006.jpg",
+      "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Beat Saber/IMG-20230706-WA0002.jpg",
+      "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Beat Saber/IMG_20230809_164434.jpg",
+      "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Cricket Experience/IMG_20230617_165033.jpg",
+      "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Rollercoaster Experience/IMG_20230121_151214.jpg",
+      "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/Walk the Plank Experience/IMG_20230809_164407.jpg",
+      "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/Walk the Plank Experience/IMG_20230809_164423.jpg",
     ],
     title: "Virtual Reality Experience",
     desc: "A VR (Virtual Reality) gaming event experience is a gathering or exhibition that allows participants to explore immersive and interactive virtual worlds using VR technology.",
@@ -94,13 +94,13 @@ const events = {
   },
   // Retro_Gaming: {
   //   img: [
-  //     "/dist/assets/Simply Gaming Event Galleria/Racing Simulator/20230811_131650.jpg",
-  //     "/dist/assets/Simply Gaming Event Galleria/Racing Simulator/IMG-20230906-WA0000.jpg",
-  //     "/dist/assets/Simply Gaming Event Galleria/Racing Simulator/IMG-20230804-WA0014.jpg",
-  //     "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Cricket Experience/IMG_20230617_165033.jpg",
-  //     "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Rollercoaster Experience/IMG_20230121_151214.jpg",
-  //     "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/Walk the Plank Experience/IMG_20230809_164407.jpg",
-  //     "/dist/assets/Simply Gaming Event Galleria/Virtual Reality Experience/Walk the Plank Experience/IMG_20230809_164423.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Racing Simulator/20230811_131650.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Racing Simulator/IMG-20230906-WA0000.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Racing Simulator/IMG-20230804-WA0014.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Cricket Experience/IMG_20230617_165033.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/VR Rollercoaster Experience/IMG_20230121_151214.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/Walk the Plank Experience/IMG_20230809_164407.jpg",
+  //     "/assets/Simply Gaming Event Galleria/Virtual Reality Experience/Walk the Plank Experience/IMG_20230809_164423.jpg",
   //   ],
   //   title: "Retro Gaming Exprience",
   //   desc: "A retro gaming event experience is a celebration of classic video games, consoles, and gaming culture from past decades. These events provide a nostalgic journey for attendees, offering the opportunity to play vintage games, explore gaming history, and connect with fellow retro gaming enthusiasts. ",
--- src/data/products.jsx
+++ src/data/products.jsx
@@ -1,98 +1,98 @@
-// import box from "/dist/assets/VR Boxing 1V1.png";
-// import race from "/dist/assets/AR RC Racing.png";
-// import arcade from "/dist/assets/Arcade Gaming.png";
-// import arsports from "/dist/assets/AR Sports.png";
-// import F1simulator from "/dist/assets/F1 Simulator.png";
-// import oculus from "/dist/assets/1.png";
-// import retrogaming from "/dist/assets/Retro Gaming.png";
-// import vrboxing from "/dist/assets/VR Boxing 1V1.png";
-// import cricket from "/dist/assets/VR Cricket.png";
-// import golf from "/dist/assets/VR Golf.png";
-// import roller from "/dist/assets/VR Rollercoaster.png";
-// import plank from "/dist/assets/VR Plank.png";
-// import img from '/dist/assets/Flight Sim.png';
+// import box from "/assets/VR Boxing 1V1.png";
+// import race from "/assets/AR RC Racing.png";
+// import arcade from "/assets/Arcade Gaming.png";
+// import arsports from "/assets/AR Sports.png";
+// import F1simulator from "/assets/F1 Simulator.png";
+// import oculus from "/assets/1.png";
+// import retrogaming from "/assets/Retro Gaming.png";
+// import vrboxing from "/assets/VR Boxing 1V1.png";
+// import cricket from "/assets/VR Cricket.png";
+// import golf from "/assets/VR Golf.png";
+// import roller from "/assets/VR Rollercoaster.png";
+// import plank from "/assets/VR Plank.png";
+// import img from '/assets/Flight Sim.png';
 
 const products = [
   {
     id: 1,
-    img: "/dist/assets/VR Cricket-05d4124d.png",
+    img: "/assets/VR Cricket-05d4124d.png",
     title: "VR Cricket",
     path: "vrcricket",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 2,
-    img: "/dist/assets/F1 Simulator-4a2e4743.png",
+    img: "/assets/F1 Simulator-4a2e4743.png",
     path: "F1simulator",
     title: "F1 Simulator VR",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 3,
-    img: "/dist/assets/Flight Sim-0a7178f4.png",
+    img: "/assets/Flight Sim-0a7178f4.png",
     title: "Flight Simulator",
     path: "Flightsimulator",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 4,
-    img: "/dist/assets/VR Plank-c02a698e.png",
+    img: "/assets/VR Plank-c02a698e.png",
     path: "Walktheplank",
     title: "Walk the Plank",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 5,
-    img: "/dist/assets/AR Sports-8fd9ae85.png",
+    img: "/assets/AR Sports-8fd9ae85.png",
     title: "AR Sports ",
     path: "arSports",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 6,
-    img: "/dist/assets/VR Rollercoaster-30db7f72.png",
+    img: "/assets/VR Rollercoaster-30db7f72.png",
     title: "VR Rollercoaster ",
     path: "Vrrollercoaster",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 7,
-    img: "/dist/assets/AR RC Racing-85c3ea18.png",
+    img: "/assets/AR RC Racing-85c3ea18.png",
     title: "AR RC Racing",
     path: "ArRacing",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 8,
-    img: "/dist/assets/Arcade Gaming-33fc34fb.png",
+    img: "/assets/Arcade Gaming-33fc34fb.png",
     title: "Arcade Gaming",
     path: "Arcadegaming",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 9,
-    img: "/dist/assets/Retro Gaming-7622eac0.png",
+    img: "/assets/Retro Gaming-7622eac0.png",
     title: "Retro Gaming",
     path: "Retrogaming",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 10,
-    img: "/dist/assets/VR Boxing 1V1-75ef058b.png",
+    img: "/assets/VR Boxing 1V1-75ef058b.png",
     title: "VR Boxing 1v1",
     path: "Vrboxing",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 11,
-    img: "/dist/assets/VR Golf-f76ce2ad.png",
+    img: "/assets/VR Golf-f76ce2ad.png",
     title: "VR Golf",
     path: "Vrgolf",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
   },
   {
     id: 12,
-    img: "/dist/assets/1-af889a08.png",
+    img: "/assets/1-af889a08.png",
     title: "Gaming PC",
     path: "Gamingpc",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
